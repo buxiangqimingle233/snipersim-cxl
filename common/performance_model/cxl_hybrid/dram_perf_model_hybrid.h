1758316c418de028e4ec94e5f943f4a82b086251
wangzhao <tusi>
2023-02-13 20:36:16 +0800

    local-first balancer

--- common/performance_model/cxl_hybrid/dram_perf_model_hybrid.h
+++ common/performance_model/cxl_hybrid/dram_perf_model_hybrid.h
@@ -6,16 +6,20 @@
 #include "fixed_types.h"
 #include "subsecond_time.h"
 #include "dram_cntlr_interface.h"
+#include "numa_balancer.h"
 
 class DramPerfModelHybrid : public DramPerfModel
 {
     private:
+        core_id_t core_id;
         QueueModel* m_queue_model;
         SubsecondTime m_dram_access_cost_local, m_dram_access_cost_remote;
         ComponentBandwidth m_dram_bandwidth;
 
         SubsecondTime m_total_queueing_delay;
         SubsecondTime m_total_access_latency;
+
+        NumaAddressBalancer* numa_balancer;
 
     public:
         DramPerfModelHybrid(core_id_t core_id, UInt32 cache_block_size);
